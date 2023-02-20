f7bd336e02350a7237f49f771a70593e831f438a
GabrielSauter <gabriel.andrade.sauter@gmail.com>
2023-02-20 15:51:05 -0300

    Call the viewModel

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteViewModel = ViewModelProvider(this)[NoteViewModel::class.java]
+        noteViewModel.allNotes.observe(this, Observer{
+            //update RecyclerView
+            Toast.makeText(this@MainActivity, "onChanged", Toast.LENGTH_SHORT).show()
+        })
     }
 }
