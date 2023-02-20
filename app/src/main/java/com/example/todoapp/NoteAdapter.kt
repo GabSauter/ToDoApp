998ea1f3450f623b472f95eb98e96968bd4ab80c
GabrielSauter <gabriel.andrade.sauter@gmail.com>
2023-02-20 16:26:15 -0300

    Created the NoteAdapter

---
+++ app/src/main/java/com/example/todoapp/NoteAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.todoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteHolder>() {
+
+    private var notes: List<Note> = ArrayList()
+
+    class NoteHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvTitle: TextView
+        var tvDescription: TextView
+        var tvPriority: TextView
+
+        init {
+            tvTitle = itemView.findViewById(R.id.tv_title)
+            tvDescription = itemView.findViewById(R.id.tv_description)
+            tvPriority = itemView.findViewById(R.id.tv_priority)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.note_item,parent,false)
+        return NoteHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    fun setNotes(notes :List<Note>){
+        this.notes = notes
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: NoteHolder, position: Int) {
+        val currNote = notes[position]
+        holder.tvTitle.text = currNote.title
+        holder.tvDescription.text = currNote.description
+        holder.tvPriority.text = currNote.priority.toString()
+    }
+
+}
