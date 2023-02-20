50f1bd386130dace03cdb0335d5f4f2f93ae4c0a
GabrielSauter <gabriel.andrade.sauter@gmail.com>
2023-02-20 10:27:12 -0300

    Created the NoteDatabase

---
+++ app/src/main/java/com/example/todoapp/NoteDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.todoapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1)
+abstract class NoteDatabase: RoomDatabase() {
+    abstract fun userDao(): NoteDao
+
+    companion object{
+        private var INSTANCE: NoteDatabase? = null
+        fun getInstance(context: Context): NoteDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE =
+                        Room.databaseBuilder(context.applicationContext, NoteDatabase::class.java, "note_database")
+                            .fallbackToDestructiveMigration()
+                            .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
